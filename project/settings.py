a3941b764d79b98e6c3ae7a42f4304b73fbe31d2
ishemapro <ishetou.123@gmail.com>
2020-08-16 12:55:56 +0200

    add sqlt3

--- project/settings.py
+++ project/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-       'ENGINE': 'django.db.backends.mysql',
-        'NAME': '3yNAbmv2h4',
-        'USER': '3yNAbmv2h4',
-        'PASSWORD': 'xG1XbUQ8cx',
-        'HOST': 'remotemysql.com',
-        'PORT': '3306',
+     'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
